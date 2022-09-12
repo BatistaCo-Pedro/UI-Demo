9351d7bd8c8a93ec834585f12280e30ec5729a8d
Pedro <pedrocorreiabatista@gmail.com>
2022-09-12 14:50:05 +0200

    snackbars?

--- app/src/main/java/com/example/ui_demo/MainActivity.kt
+++ app/src/main/java/com/example/ui_demo/MainActivity.kt
@@ -4,15 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_demo_activity)
         setListeners();
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        val myLinearLayout = findViewById<LinearLayout>(R.id.linearLayout)
+        Snackbar.make(myLinearLayout, "${item.title} wurde geclickt", Snackbar.LENGTH_LONG).show()
+        return true
     }
 
 
